f2dfacd17092d9bf6c9abfd3e1e2bf0ca56d7a22
Katy Day <day.katy@gmail.com>
2021-01-05 16:24:57 +0000

    chapter 8 - adding release_bike method

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,5 +1,8 @@
 require 'docking_station'
 
 describe DockingStation do 
+# it "should respond to release_bike" do 
+# expect(docking_station).to respond_to(:release_bike)
+it { is_expected.to respond_to(:release_bike) }
 
 end
